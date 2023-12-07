443e1c6fcaa6106e89970d5b9a85916e5ba4247f
k-mughal <appsub@yahoo.ie>
2023-12-07 22:47:41 +0000

    gitignore file added, .env file added along db variables

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -10,7 +10,11 @@ app = Flask(__name__)
 load_dotenv()
 CORS(app)  # Enable CORS for all routes
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789*@localhost:5432/flask_db'
+# QLAlchemy database URI
+SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 db = SQLAlchemy(app)
 
